a3b497ce287818d0850649afdcb88a6e3bb7d4e4
jordanvieira <marvaojordan98@gmail.com>
2022-07-06 14:16:50 -0300

    adicionando create e read

--- APIS/CRUD/app/views.py
+++ APIS/CRUD/app/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.forms import CarrosForm
+from app.models import Carros
 
 
 
 def home(request):
-    return render(request, 'index.html')
+    data = {}
+    data['db']= Carros.objects.all()
+    return render(request, 'index.html',data)
 
 
 def form(request):
     data = {}
     data['form'] = CarrosForm()
     return render(request, 'form.html', data)
+
+
+def create(request):
+    form = CarrosForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+def view(resquest, pk):
+    data={}
+    data['db'] = Carros.objects.get(pk=pk)
+    return render(resquest, 'view.html', data)
