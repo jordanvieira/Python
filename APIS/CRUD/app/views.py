363b95b28ad405a7c341a05356af93a6c94a519e
jordanvieira <marvaojordan98@gmail.com>
2022-07-07 00:35:12 -0300

    adicionando update, delete, search

--- APIS/CRUD/app/views.py
+++ APIS/CRUD/app/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect
 from app.forms import CarrosForm
 from app.models import Carros
-
+from django.core.paginator import Paginator
 
 
 def home(request):
     data = {}
-    data['db']= Carros.objects.all()
-    return render(request, 'index.html',data)
+    search = request.GET.get('search')
+    if search:
+        data['db'] = Carros.objects.filter(Modelo__icontains=search)
+    else:
+        data['db'] = Carros.objects.all()
+    # all = Carros.objects.all()
+    # paginator = Paginator(all, 2)
+    # pages = request.GET.get('page')
+    # data['db']= paginator.get_page(pages)
+    return render(request, 'index.html', data)
 
 
 def form(request):
@@ -22,7 +30,30 @@ def create(request):
         form.save()
         return redirect('home')
 
+
 def view(resquest, pk):
-    data={}
+    data = {}
     data['db'] = Carros.objects.get(pk=pk)
     return render(resquest, 'view.html', data)
+
+
+def edit(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    data['form'] = CarrosForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+
+def update(request, pk):
+    data = {}
+    data['db'] = Carros.objects.get(pk=pk)
+    form = CarrosForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+
+def delete(request, pk):
+    db = Carros.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
